e12b9bbb07791af7289dfdddf959b8fe35471a1c
Ondrej Kabrt <kabrtondra@gmail.com>
2023-11-15 09:11:45 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function scitani(a,b){
+    return a + b;
+}
+
+function odcitani(a,b){
+    return a - b;
+}
+
+function nasobeni(a,b){
+    return a * b;
+}
+
+function deleni(a,b){
+    return a / b;
+}
+
+function sin(a){
+    return Math.sin(a);
+}
+function cos(a){
+    return Math.cos(a);
+}
+function tg(a){
+    return Math.tan(a);
+}
+function cotg(a){
+    return Math.cotg(a);
+}
+function Log(a){
+    return Math.log(a);
+}
+function exponent(a,b){
+    return a ** b;
+}
+function Odmocnina(a){
+    return Math.sqrt(a);
+}
+
+
